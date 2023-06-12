b6f110c8540b97e4115fba08cd6b5f2696816e35
jessLijie <wong.lijie1105@gmail.com>
2023-06-13 00:08:55 +0800

    Committing local changes before switching to jess branch

--- database/createdb.php
+++ database/createdb.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost", "root", "");
+$con = mysqli_connect("localhost", "root", "jkty12138");
 if (!$con) {
 
     die('Could not connect: ' . mysqli_connect_error());
