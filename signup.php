ae9206fdce702b62657bd987e740c7e1f48f2b7e
nqianhuiii <nghui@graduate.utm.my>
2023-06-27 21:03:37 +0800

    create sign up input validation

--- signup.php
+++ signup.php
@@ -38,34 +38,38 @@
         <div class="col-md-6">
             <div class="card">
                 <div class="card-body">
-                    <form action="sql/signup_form.php" method="POST">
+                    <form id="signup-form" action="sql/signup_form.php" method="POST" onsubmit="return validateForm()">
                         <div>
                             <h4>Candidate Sign Up</h4>
                         </div>
                         <input type="hidden" name="type" value="1">
                         <div class="form-group">
                             <label for="username">Username</label>
-                            <input type="text" class="form-control" id="username" name="username">
+                            <input type="text" class="form-control" id="username" name="username" required>
                         </div>
                         <div class="form-group">
                             <label for="phoneNo">Phone Number</label>
-                            <input type="tel" class="form-control" id="phoneNo" name="phoneNo">
+                            <input type="tel" class="form-control" id="phoneNo" name="phoneNo" required>
+                            <p id="phoneNo-error" style="display: none; color: red; font-size: 10px">Invalid phone number format</p>
                         </div>
                         <div class="form-group">
                             <label for="email">Email</label>
                             <input type="email" class="form-control" id="email" name="email">
+                            <p id="email-error" style="display: none; color: red; font-size: 10px">Invalid email format</p>
                         </div>
                         <div class="form-group">
                             <label for="password">Password</label>
-                            <input type="password" class="form-control" id="password" name="password">
+                            <input type="password" class="form-control" id="password" name="password" required>
+                            <p id="password-error" style="display: none; color: red; font-size: 10px">Password must be between 8-15 characters</p>
                         </div>
                         <div class="form-group">
                             <label for="confirmPassword">Confirm Password</label>
-                            <input type="password" class="form-control" id="confirmPassword" name="confirmPassword">
+                            <input type="password" class="form-control" id="confirmPassword" name="confirmPassword" required>
+                            <p id="confirmPassword-error" style="display: none; color: red; font-size: 10px">Password does not match</p>
                         </div>
                         <button type="submit" class="btn btn-primary btn-block btn-black">Sign Up</button>
-                        <div class= "text-center mt-3">
-                            <h>Already sign up? <a href= "signin.php" >Sign In Now</a></h>
+                        <div class="text-center mt-3">
+                            <h>Already sign up? <a href="signin.php">Sign In Now</a></h>
                         </div>
                     </form>
                 </div>
@@ -73,7 +77,70 @@
         </div>
     </div>
 </div>
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const emailInput = document.getElementById("email");
+        const emailErrorMessage = document.getElementById("email-error");
 
-  
-</body>
-</html>
+        emailInput.addEventListener("blur", function() {
+            const emailValue = emailInput.value;
+
+            // Validate email format
+            if (!validateEmail(emailValue)) {
+                emailErrorMessage.style.display = "block";
+            } else {
+                emailErrorMessage.style.display = "none";
+            }
+        });
+
+        document.getElementById("signup-form").addEventListener("submit", function(event) {
+            const emailValue = emailInput.value;
+            const passwordValue = document.getElementById("password").value;
+            const confirmPasswordValue = document.getElementById("confirmPassword").value;
+
+            // Validate email format
+            if (!validateEmail(emailValue)) {
+                emailErrorMessage.style.display = "block";
+                event.preventDefault(); // Prevent form submission
+            } else {
+                emailErrorMessage.style.display = "none";
+            }
+
+            // Check password length
+            if (passwordValue.length < 8 || passwordValue.length > 15) {
+                document.getElementById("password-error").style.display = "block";
+                event.preventDefault(); // Prevent form submission
+            } else {
+                document.getElementById("password-error").style.display = "none";
+            }
+
+            // Check if passwords match
+            if (passwordValue !== confirmPasswordValue) {
+                document.getElementById("confirmPassword-error").style.display = "block";
+                event.preventDefault(); // Prevent form submission
+            } else {
+                document.getElementById("confirmPassword-error").style.display = "none";
+            }
+        });
+
+        const phoneInput = document.getElementById("phoneNo");
+        phoneInput.addEventListener("input", function () {
+            const phoneValue = phoneInput.value;
+
+            // Remove non-numeric characters from the input value
+            const numericValue = phoneValue.replace(/\D/g, "");
+
+            // Display error message if the input value is not equal to the numeric value
+            if (phoneValue !== numericValue) {
+                document.getElementById("phoneNo-error").style.display = "block";
+            } else {
+                document.getElementById("phoneNo-error").style.display = "none";
+            }
+        });
+
+        function validateEmail(email) {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return emailRegex.test(email);
+        }
+    });
+</script>
